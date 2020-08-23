458ea71f7150be8f9963ed4990a94c5ec44f470d
Unnati16-dv <62612774+Unnati16-dv@users.noreply.github.com>
2020-08-23 23:16:48 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,68 @@
+<?php
+   define('DB_SERVER', 'localhost:3036');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', 'rootpassword');
+   define('DB_DATABASE', 'database');
+   
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // To send username and password from form 
+      
+      $user = mysqli_real_escape_string($db,$_POST['username']);
+      $pass = mysqli_real_escape_string($db,$_POST['password']); 
+      
+      $sql = "SELECT id FROM admin WHERE username = '$user' and passcode = '$pass'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If user found in the database $result will have one row from table
+		
+      if($count == 1) {
+         session_register("user");
+         $_SESSION['login_user'] = $user;
+         
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
+?>
+<!DOCTYPE html>
+<html>
+   
+   <head>
+      <title>Login Page</title>
+      
+      
+   </head>
+   
+   <body bgcolor = "#FFFFFF">
+	
+      <div align = "center">
+         <div style = "width:300px; border: solid 1px #333333; " align = "left">
+            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+				
+            <div style = "margin:30px">
+               
+               <form action = "" method = "post">
+                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
+                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <input type = "submit" value = " Submit "/><br />
+               </form>
+               
+               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
+					
+            </div>
+				
+         </div>
+			
+      </div>
+
+   </body>
+</html>
